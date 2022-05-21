dbedfede7c456444432a4bc8678a8ac59c31fef6
Дима Киреев <dimanoit@gmail.com>
2022-05-21 21:38:43 +0300

    feat: add logging

--- Soulgram.Posts.Application/ServiceInjector.cs
+++ Soulgram.Posts.Application/ServiceInjector.cs
@@ -1,17 +1,35 @@
 ﻿using System.Reflection;
 using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Soulgram.Logging;
+using Soulgram.Logging.Models;
 
 namespace Soulgram.Posts.Application;
 
 public static class ServiceInjector
 {
-    public static IServiceCollection AddApplicationLayerDependencies(this IServiceCollection services)
+    public static IServiceCollection AddApplicationLayerDependencies(this IServiceCollection services,
+        IConfiguration configuration)
     {
         var currentAssembly = Assembly.GetExecutingAssembly();
         services.AddMediatR(currentAssembly);
         services.AddValidatorsFromAssembly(currentAssembly);
+        services.AddLogging(configuration);
+        
         return services;
+    }
+
+
+    private static IServiceCollection AddLogging(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var loggingSettings = configuration
+            .GetSection("LoggingSettings")
+            .Get<LoggingSettings>();
+
+        return services.AddLogging(loggingSettings);
     }
 }
